e8122f3c49a766f220bd3ba95d60523b155781b5
Eyobel Haile <eyoh360@gmail.com>
2023-07-07 23:20:50 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+//Bella Haile, IT211, Date:07/07/2023
+
+
 function translatePigLatin(str){
+    // Check if the input is empty or a single-letter word
     str = str.toLowerCase();
     let initialVowel = str.match(/[aeiou]/);
     let initialConsonant = str. match(/[bcdfghjklmnpqrstvwxyz]/);
@@ -6,22 +10,23 @@ function translatePigLatin(str){
     if (!input || input.length === 1) {
         return "Input cannot be translated.";
       }
+      // Split the input into an array of words
       const words = input.split(" ");
 
       const translatedWords = words.map((word) => {
         const lowercaseWord = word.toLowerCase();
-    
+    //check if a character is a vowel
         if (initialVowel(lowercaseWord[0])) {
           return lowercaseWord + "way";
         }
-
+    // Check if the word starts with a consonant
         if (initialConsonant(lowercaseWord[0])) {
             if (initialConsonant(lowercaseWord[1])) {
               return lowercaseWord.slice(2) + lowercaseWord.slice(0, 2) + "ay";
             }
             return lowercaseWord.slice(1) + lowercaseWord[0] + "ay";
         }
-
+    // If the word doesn't start with a vowel or consonant, return it as is
         return word;
 });
 
